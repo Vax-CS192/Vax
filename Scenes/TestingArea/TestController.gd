32b0dc28ba11ee435fa8eb9bb801e140f9123e05
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2022-02-27 20:32:55 +0800

    TestingArea Added, Docs for Cauldron updated

--- Scenes/TestingArea/TestController.gd
+++ Scenes/TestingArea/TestController.gd
@@ -1,13 +1,19 @@
-
 # Author: Jeremy Vincent Yu
 # License: 0BSD
 # Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
 
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
-signal back_to_lab
-extends Node2D
 
 
+extends Node
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
@@ -16,9 +22,6 @@ func _ready():
 #func _process(delta):
 #	pass
 
-#  This method causes the CauldronUI to signal to the cauldron controller to instance the Lab
-#  subsystem and remove the Cauldron subsystem from memory.
-func _on_Back_pressed():
-	var cauldron_exterior = get_parent()
-	print("Back Pressed")
-	emit_signal("back_to_lab")
+#This changes the scene to the Lab.
+func _on_TestingAreaUI_back_pressed():
+	get_tree().change_scene("res://Scenes/Lab/Lab.tscn")
