83b373916bb916633fdfb9023a3b105f982722ca
John Henry Galino <jhgalino@disroot.org>
2022-03-02 14:55:22 +0800

    Added illustrations, connected some scenes to lab

--- Scenes/Lab/Lab.gd
+++ Scenes/Lab/Lab.gd
@@ -1,5 +1,8 @@
-extends Node
+extends Control 
 
+func _ready():
+	$LabBG.position.x = get_viewport_rect().size.x / 2
+	$LabBG.position.y = get_viewport_rect().size.y / 4
 
 
 func _on_Formula1_pressed():
@@ -8,3 +11,15 @@ func _on_Formula1_pressed():
 
 func _on_Formula2_pressed():
 	get_tree().change_scene("res://Scenes/Shop/Shop.tscn")
+
+
+func _on_BackToMainMenu_pressed():
+	get_tree().change_scene("res://Scenes/MainMenu/MainMenu.tscn")
+
+
+func _on_Formula3_pressed():
+	print('Wala pang scene')
+
+
+func _on_Formula4_pressed():
+	print('Wala pang scene')
