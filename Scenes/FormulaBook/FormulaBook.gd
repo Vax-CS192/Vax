0ad0dbf7fa40f84065ce85b3c7bf07925cb1f87e
ameaaira00 <aealoveros@up.edu.ph>
2022-03-04 14:19:36 +0800

    Added license. Fixed Links

--- Scenes/FormulaBook/FormulaBook.gd
+++ Scenes/FormulaBook/FormulaBook.gd
@@ -1,9 +1,20 @@
+# Author: Aira Mae Aloveros
+# License: 0BSD
+# Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS 
+# SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE 
+# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 
+# OF THIS SOFTWARE.
+
 extends Node
 
-var path
-
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+# The formula name and state are set here.
 func _process(delta):
-	#Set Formula State and Names
+	#Set Formula State and Name
 	$FormulaBookControl/Formula1.is_occupied=true
 	get_node("FormulaBookControl/Formula1/Name").text="NGALAN"
 	$FormulaBookControl/Formula2.is_occupied=true
