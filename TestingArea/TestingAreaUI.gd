8002836141e3da4d1bfdda6b0fe5e3ada46fcf5d
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2022-03-16 13:57:34 +0800

    Restored Lost Assets

---
+++ TestingArea/TestingAreaUI.gd
@@ -0,0 +1,28 @@
+# Author: Jeremy Vincent Yu
+# License: 0BSD
+# Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+
+extends Node2D
+signal back_pressed
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#  This method causes the TestingAreaUI to signal to TestController to instance the Lab
+#  subsystem and remove the TestingArea subsystem from memory.
+func _on_back_pressed():
+	emit_signal("back_pressed")
