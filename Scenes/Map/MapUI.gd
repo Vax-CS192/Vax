64028bfc4ee0c29f163daec3b9f5490ccfd0068a
Kurt Ian Khalid Israel <kiisrael@up.edu.ph>
2022-03-17 16:01:30 +0800

    added the save mechanism of president's name and made it such that map shows the saved money

--- Scenes/Map/MapUI.gd
+++ Scenes/Map/MapUI.gd
@@ -4,24 +4,10 @@
 
 # THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
-extends Node
+extends Control
 
-onready var input_box = $UI/InputBox
-onready var enter_button = $UI/EnterButton
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var money_amount = $Money/Account
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if input_box.text == "":
-		enter_button.disabled = true
-	else:
-		enter_button.disabled = false
-
-# This changes the scene to the Lab.
-func _on_EnterButton_pressed():
-	Profile.president_name = input_box.text
-	Profile.save_president_name()
-	get_tree().change_scene("res://Scenes/Lab/Lab.tscn")
+	money_amount.text = "PHP %s" % [Profile.format_money(Profile.money)]
