75be117a45efc4be79b862f9ce265da2567c8271
John Henry Galino <jhgalino@disroot.org>
2022-03-28 22:09:24 +0800

    Added comments to Session.gd

--- Scenes/Shared/VirusGenerator.gd
+++ Scenes/Shared/VirusGenerator.gd
@@ -1,16 +1,7 @@
 extends Node
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 class_name VirusGenerator
+
 
 var alphabet = {
 	"0": "t",
