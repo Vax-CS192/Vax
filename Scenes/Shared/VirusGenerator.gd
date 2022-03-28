276ea8e6bdde52c5616266254992faf9d067882e
John Henry Galino <jhgalino@disroot.org>
2022-03-28 23:39:30 +0800

    Add comments and license

--- Scenes/Shared/VirusGenerator.gd
+++ Scenes/Shared/VirusGenerator.gd
@@ -1,8 +1,21 @@
+# Author: John Henry A. Galino
+# License: 0BSD
+# Permission to use, copy, modify, and/or distribute this software for any purpose with or without 
+# fee is hereby granted.
+
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS 
+# SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE 
+# AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES 
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, 
+# NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE 
+# OF THIS SOFTWARE.
+
 extends Node
 
+# This class provides methods to generate Symptoms and Bundles
 class_name VirusGenerator
 
-
+# letters that compose the symptoms and bundles
 var alphabet = {
 	"0": "t",
 	"1": "a",
@@ -10,6 +23,8 @@ var alphabet = {
 	"3": "g",
 }
 
+# generate Symptom. This returns a string made of "t", "a", "c", and "g" with
+# parameter length
 func generateSymptom(length: int) -> String:
 	randomize()
 	var symp = ""
@@ -18,6 +33,10 @@ func generateSymptom(length: int) -> String:
 		symp += alphabet.get(String(random))
 	return symp
 
+# generate bundles. baseSymp is the actual symptom, while genBund1, genBund2, and
+# genBund3 are derived from it. baseSymp is supposed to be the bundle that cures
+# the symptom by 100%, which is why it is the symptom as well. genBund1, genBund2, and
+# genBund3 are bundles with less efficiency than baseSymp
 func generateBundles() -> Array:
 	var baseSymp: String = generateSymptom(10)
 	var genBund1 = baseSymp.substr(0,5) + generateSymptom(5)
