cdca21fb5e25cfe1271423990aa2d3bd94c4a1cc
ameaaira00 <aealoveros@up.edu.ph>
2022-03-30 00:19:15 +0800

    Archives update & deletion

--- Scenes/FormulaBook/Archive Page/ArchivePopup/ArchivePopup.gd
+++ Scenes/FormulaBook/Archive Page/ArchivePopup/ArchivePopup.gd
@@ -12,6 +12,8 @@
 
 
 extends Popup
+signal delete_an_archive(id)
+signal archive_deets_edited(formula_parameters)
 
 onready var enabled_fav_button = preload("res://Assets/Formula Book/Archive Page/Archive FormulaPopup/Set as Favorite Button enabled.png")
 onready var disabled_fav_button = preload("res://Assets/Formula Book/Archive Page/Archive FormulaPopup/Set as Favorite Button.png")
@@ -29,29 +31,46 @@ onready var formula_parameters := {
 # var b = "text"
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	$ArchivePopupControl/Slots/Slot1.is_occupied=true
-	$ArchivePopupControl/Slots/Slot2.is_occupied=true
-	$ArchivePopupControl/Slots/Slot3.is_occupied=true
-	$ArchivePopupControl/Slots/Slot4.is_occupied=true
-	$ArchivePopupControl/Slots/Slot5.is_occupied=true
-	$ArchivePopupControl/Slots/Slot6.is_occupied=true
-	$ArchivePopupControl/Slots/Slot7.is_occupied=true
-	$ArchivePopupControl/Slots/Slot8.is_occupied=true
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+#func _process(delta):
 	
 	#Fix this hardcoded code
-	if $ArchivePopupControl/Slots/Slot9.is_pressed==true || $ArchivePopupControl/Slots/Slot10.is_pressed==true:
-		$ArchivePopupControl/SetAsFavoriteButton.texture_normal=enabled_fav_button
-		if $ArchivePopupControl/Slots/Slot9.is_pressed==true:
-			$ArchivePopupControl/Slots/Slot10.is_disabled=true              
-		elif $ArchivePopupControl/Slots/Slot10.is_pressed==true:
-			$ArchivePopupControl/Slots/Slot9.is_disabled=true 
-	else:
-		$ArchivePopupControl/Slots/Slot9.is_disabled=false
-		$ArchivePopupControl/Slots/Slot10.is_disabled=false
-		$ArchivePopupControl/SetAsFavoriteButton.texture_normal=disabled_fav_button
+#	if $ArchivePopupControl/Slots/Slot9.is_pressed==true || $ArchivePopupControl/Slots/Slot10.is_pressed==true:
+#		$ArchivePopupControl/SetAsFavoriteButton.texture_normal=enabled_fav_button
+#		if $ArchivePopupControl/Slots/Slot9.is_pressed==true:
+#			$ArchivePopupControl/Slots/Slot10.is_disabled=true              
+#		elif $ArchivePopupControl/Slots/Slot10.is_pressed==true:
+#			$ArchivePopupControl/Slots/Slot9.is_disabled=true 
+#	else:
+#		$ArchivePopupControl/Slots/Slot9.is_disabled=false
+#		$ArchivePopupControl/Slots/Slot10.is_disabled=false
+#		$ArchivePopupControl/SetAsFavoriteButton.texture_normal=disabled_fav_button
+
+
+func _on_Popup_about_to_show(new_formula_parameters: Dictionary,fav_count:int):
+	formula_parameters = new_formula_parameters
+	$ArchivePopupControl/FormulaName.text=formula_parameters["Name"]
+	$ArchivePopupControl/FormulaNote.text= formula_parameters["Description"]
+	for i in range(10):
+		var slot_path="ArchivePopupControl/Slots/Slot"+str(i+1)
+		if i < fav_count:
+			get_node(slot_path).is_occupied=true
+		else:
+			get_node(slot_path).is_occupied=false
+	self.popup_centered()
+
+func synch_formula_parameters():
+	formula_parameters["Name"]=$ArchivePopupControl/FormulaName.text
+	formula_parameters["Description"]=$ArchivePopupControl/FormulaNote.text
+	
+#Emits signal
+func _on_DeleteButton_pressed():
+	emit_signal("delete_an_archive",formula_parameters["ID"])
+	self.hide()
+
+
+func _on_Popup_popup_hide():
+	synch_formula_parameters()
+	emit_signal("archive_deets_edited", formula_parameters)
+	self.hide() # Replace with function body.
