ef19606493227047074c922fd29c8ed3568fe544
Jeremy Vincent Yu <jtyu3@up.edu.ph>
2022-04-19 08:44:42 +0800

    Activated testing Area

--- Scenes/TestingArea/TestingAreaUI.gd
+++ Scenes/TestingArea/TestingAreaUI.gd
@@ -7,7 +7,10 @@
 
 extends Node2D
 signal back_pressed
-
+var selected_patient = -1
+var patient_vaccines = [-1,-1,-1,-1,-1]
+var max_vaccine = 0
+var pretest = true
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,8 +19,6 @@ signal back_pressed
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-
-
 
 #func _process(delta):
 
@@ -32,3 +33,49 @@ func _on_back_pressed():
 func draw():
 		get_node("money_bg/money_value").text = "PHP " + Profile.format_money(Profile.money)
 		self.show()
+
+#This method sets the selected patient to 0, then shows ReagentOverlay
+func _on_patient1_pressed():
+	if pretest:
+		selected_patient = 0
+		$ReagentHolder._on_patient_pressed()
+	
+#This method sets the selected patient to 1, then shows ReagentOverlay
+func _on_patient2_pressed():
+	if pretest:
+		selected_patient = 1
+		$ReagentHolder._on_patient_pressed()
+	
+#This method sets the selected patient to 2, then shows ReagentOverlay
+func _on_patient3_pressed():
+	if pretest:
+		selected_patient = 2
+		$ReagentHolder._on_patient_pressed()
+	
+#This method sets the selected patient to 3, then shows ReagentOverlay
+func _on_patient4_pressed():
+	if pretest:
+		selected_patient = 3
+		$ReagentHolder._on_patient_pressed()
+
+#This method sets the selected patient to 4, then shows ReagentOverlay
+func _on_patient5_pressed():
+	if pretest:
+		selected_patient = 4
+		$ReagentHolder._on_patient_pressed()
+
+#This sets the vaccine associated with a patient
+func set_vaccine(selected_vaccine: int):
+	var patient_reference = $Labels.get_node("p"+str(selected_patient))
+	if selected_vaccine <= max_vaccine:
+		patient_vaccines[selected_patient] = selected_vaccine
+	else:	
+		patient_vaccines[selected_patient] = - 1
+		patient_reference.text = "Blank"
+
+#Reset TestingArea to initial State
+func _on_reset_pressed():
+	pretest  = true
+	for x in range(-5):
+		patient_vaccines[x] = -1
+		$Labels.get_node("p"+str(selected_patient)).text= "Blank"
